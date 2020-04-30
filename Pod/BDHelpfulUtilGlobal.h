780d9eaef52aedfa2ff751a7f9a8fe028994671a
Lay <lay.zhang@beibei.com>
2020-04-30 16:27:07 +0800

    add

---
+++ Pod/BDHelpfulUtilGlobal.h
@@ -0,0 +1,24 @@
+//
+//  BDHelpfulUtilGlobal.h
+//  BDHelpfulUtil
+//
+//  Created by Lay on 2020/4/8.
+//
+
+#ifndef BDHelpfulUtilGlobal_h
+#define BDHelpfulUtilGlobal_h
+
+#import "HBURLConvertible.h"
+#import "UIColor+BDHex.h"
+#import "YYText.h"
+#import "BlocksKit.h"
+#import "SDWebImageManager.h"
+#import "extobjc.h"
+#import "UIView+BDViewFrame.h"
+#import "UIImageView+WebCache.h"
+#import "UIImage+BDResize.h"
+#import "UIResponder+Event.h"
+
+#define STRING(str)  ((str) != nil ? [NSString stringWithFormat:@"%@", (str)] : @"")
+
+#endif /* BDAttributeMakerGlobal_h */
