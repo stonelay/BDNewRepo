cbc99b2196168cb528e8f3ff84fc5e9adc82aea3
Lay <lay.zhang@beibei.com>
2020-05-27 11:00:35 +0800

    add new

---
+++ BDNewRepo/Classes/model/NewRModel.m
@@ -0,0 +1,16 @@
+//
+//  NewRModel.m
+//  BDNewRepo
+//
+//  Created by Lay on 2020/5/27.
+//
+
+#import "NewRModel.h"
+
+@implementation NewRModel
+
+- (void)rModel
+{
+    NSLog(@"print");
+}
+@end
